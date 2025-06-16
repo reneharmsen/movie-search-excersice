e91550a4e2db90b3e2b0e843159b0d1920a45b2a
Rene Harmsen <rene@emakina.nl>
2025-06-16 14:25:45 +0200

    yepyep

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -7,12 +7,14 @@ type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-
 export default async function MoviePage({ params }: Props) {
     const request_headers = await headers();
-    const host = request_headers.get('host') || 'localhost:3000';
 
-    const res = await fetch(`http://${host}/movie/?id=${(await params).id}`);
+    const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+    const host = request_headers.get('host') ?? 'localhost:3000';
+    const baseUrl = `${protocol}://${host}`;
+
+    const res = await fetch(`${baseUrl}/movie/?id=${(await params).id}`);
     const movie: Movie = await res.json();
 
     return (
