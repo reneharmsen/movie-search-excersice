feb4df9e022b9244fc97e1529e09917016d88739
Rene Harmsen <rene.harmsen@gmail.com>
2025-06-16 17:34:13 +0200

    sdffsdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Movie Search',
     description: 'Search for movies and series',
-};
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" data-theme="corporate" className="bg-secondary">
             <body className={inter.className}>
-                
-                JAJA
-                
+                NEENEE
                 {children}</body>
         </html>
     );
